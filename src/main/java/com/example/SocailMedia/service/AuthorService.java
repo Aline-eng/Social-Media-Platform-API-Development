0ecf367e6cd46af3e6f92791a4fca399d7aa841a
Aline-eng <alinenzikwinkunda@gmail.com>
2026-06-22 22:47:47 +0200

    Implemented the business logic classes for our project

--- src/main/java/com/example/SocailMedia/service/AuthorService.java
+++ src/main/java/com/example/SocailMedia/service/AuthorService.java
@@ -1,4 +1,46 @@
 package com.example.SocailMedia.service;
 
+import com.example.SocailMedia.dto.AuthorResponse;
+import com.example.SocailMedia.dto.CreateAuthorRequest;
+import com.example.SocailMedia.model.Author;
+import com.example.SocailMedia.repository.AuthorRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class AuthorService {
+    private final AuthorRepository authorRepository;
+    public AuthorResponse createAuthor(CreateAuthorRequest request) {
+        Author author = new Author();
+        author.setFullName(request.getFullName());
+        author.setUsername(request.getUsername());
+        author.setEmail(request.getEmail());
+        author.setCreatedAt(LocalDateTime.now());
+        Author saved = authorRepository.save(author);
+        AuthorResponse response = new AuthorResponse();
+        response.setMessage("Author created successfully");
+        response.setFullName(saved.getFullName());
+        response.setUsername(saved.getUsername());
+        response.setEmail(saved.getEmail());
+        response.setCreatedAt(saved.getCreatedAt());
+
+        return response;
+    }
+    public List<Author> getAllAuthors() {
+        return authorRepository.findAll();
+    }
+    public Author getAuthorById(Long id) {
+        return authorRepository.findById(id).orElse(null);
+    }
+    public Author getAuthorByUsername(String username) {
+        return authorRepository.findByUsername(username).orElse(null);
+    }
+    public Author getAuthorByEmail(String email) {
+        return authorRepository.findByEmail(email).orElse(null);
+    }
+
 }
--- src/main/java/com/example/SocailMedia/service/PostService.java
+++ src/main/java/com/example/SocailMedia/service/PostService.java
@@ -1,4 +1,40 @@
 package com.example.SocailMedia.service;
 
+import com.example.SocailMedia.dto.CreatePostRequest;
+import com.example.SocailMedia.dto.PostResponse;
+import com.example.SocailMedia.model.Author;
+import com.example.SocailMedia.model.Post;
+import com.example.SocailMedia.repository.AuthorRepository;
+import com.example.SocailMedia.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
 public class PostService {
+    private final PostRepository postRepository;
+    private final AuthorRepository authorRepository;
+
+    public PostResponse createPost(CreatePostRequest request) {
+        Author author = authorRepository.findById(request.getAuthorId()).orElseThrow(() -> new RuntimeException("Author not found"));
+        Post post = new Post();
+        post.setTitle(request.getTitle());
+        post.setContent(request.getContent());
+        post.setVisibility(request.getVisibility());
+        post.setCreatedBy(author);
+        post.setCreatedAt(LocalDateTime.now());
+
+        Post saved = postRepository.save(post);
+        PostResponse response = new PostResponse();
+        response.setTitle(saved.getTitle());
+        response.setContent(saved.getContent());
+        response.setVisibility(saved.getVisibility());
+        response.setCreatedAt(saved.getCreatedAt());
+        response.setCreatedBy(author.getFullName());
+
+        return response;
+
+    }
 }
