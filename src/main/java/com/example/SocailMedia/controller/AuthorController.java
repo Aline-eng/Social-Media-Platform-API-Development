3261eb9693358a911786f64a3691d9751601c0b5
Aline-eng <alinenzikwinkunda@gmail.com>
2026-06-22 22:47:03 +0200

    Created the controllers for the endpoints

--- src/main/java/com/example/SocailMedia/controller/AuthorController.java
+++ src/main/java/com/example/SocailMedia/controller/AuthorController.java
@@ -1,4 +1,31 @@
 package com.example.SocailMedia.controller;
 
+import com.example.SocailMedia.dto.AuthorResponse;
+import com.example.SocailMedia.dto.CreateAuthorRequest;
+import com.example.SocailMedia.model.Author;
+import com.example.SocailMedia.service.AuthorService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/authors")
+@RequiredArgsConstructor
 public class AuthorController {
+    private final AuthorService authorService;
+    @PostMapping
+    public AuthorResponse createAuthor(@Validated @RequestBody CreateAuthorRequest request) {
+        return authorService.createAuthor(request);
+    }
+    @GetMapping
+    public List<Author> getAllAuthors() {
+        return authorService.getAllAuthors();
+    }
+    @GetMapping("/{id}")
+    public Author getAuthorById(@PathVariable Long id){
+        return authorService.getAuthorById(id);
+    }
+
 }
--- src/main/java/com/example/SocailMedia/controller/PostController.java
+++ src/main/java/com/example/SocailMedia/controller/PostController.java
@@ -1,4 +1,23 @@
 package com.example.SocailMedia.controller;
 
+
+import com.example.SocailMedia.dto.CreatePostRequest;
+import com.example.SocailMedia.dto.PostResponse;
+import com.example.SocailMedia.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/posts")
+@RequiredArgsConstructor
 public class PostController {
+    private final PostService postService;
+    @PostMapping
+    public PostResponse createPost(@Validated @RequestBody CreatePostRequest request) {
+        return postService.createPost(request);
+    }
 }
